230ee7cf7ed0ffdd4208fca663cc36038097ffd0
Nishtha Sharma <nishtha.idr@gmail.com>
2019-11-15 23:46:36 +0530

    blog

--- Tedwebsite/blog/views.py
+++ Tedwebsite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Blog home page")
+# # Create your views here.
+# def index(request):
+#     return HttpResponse("Blog home page")
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
