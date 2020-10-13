bb26dc0187a53b505c9470e7588b313a8b89a7fa
Shivangi Kumar <ireneholmes221999@gmail.com>
2020-10-14 02:30:02 +0530

    fixed speaker links

--- Tedwebsite/speakers/urls.py
+++ Tedwebsite/speakers/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.speakers_home, name='speakers_home'),
-    path('speaker1', views.speakers_home, name='speakers_home'),
-    path('/speaker21', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker22', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker23', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker24', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker25', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker26', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker27', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker11', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker12', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker13', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker14', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker15', views.speakerDesc, name = 'speaker_description'),
-    path('/speaker16', views.speakerDesc, name = 'speaker_description')
-    
+    path('/Alana_Golmei', views.speakerDesc1, name='speaker_description1'),
+    path('/Anamika_Barua', views.speakerDesc2, name='speaker_description2'),
+    path('/Arup_Kumar_Dutta', views.speakerDesc3, name='speaker_description3'),
+    path('/Binita_Jain', views.speakerDesc4, name='speaker_description4'),
+    path('/Milin_Dutta', views.speakerDesc5, name='speaker_description5'),
+    path('/Zoma_Sailo', views.speakerDesc6, name='speaker_description6'),
+    path('/Pragnya_Ramjee', views.speakerDesc7, name='speaker_description7'),
+
+    path('/Seema_Biswas', views.speakerDesc8, name='speaker_description8'),
+    path('/Uddhab_Bharali', views.speakerDesc9, name='speaker_description9'),
+    path('/Sankara_Subramaniam', views.speakerDesc10, name='speaker_description10'),
+    path('/Hasina_Kharbhih', views.speakerDesc11, name='speaker_description11'),
+    path('/Sonjoy_Hazarika', views.speakerDesc12, name='speaker_description12'),
+    path('/Ravindranath_Ravi', views.speakerDesc13, name='speaker_description13'),
 
     # path('add_speaker', views.addSpeaker),
 ]
--- Tedwebsite/speakers/views.py
+++ Tedwebsite/speakers/views.py
@@ -21,23 +21,58 @@ def addSpeaker(request):
 def nominate_yourself(request):
     return render(request, 'speakers/form2.html')
 
-def speakerDesc(request):
+
+def speakerDesc1(request):
     return render(request, 'speakers/speaker21.html')
 
-def speakerDesc(request):
+
+def speakerDesc2(request):
     return render(request, 'speakers/speaker22.html')
-    
-def speakerDesc(request):
+
+
+def speakerDesc3(request):
+    return render(request, 'speakers/speaker23.html')
+
+
+def speakerDesc4(request):
+    return render(request, 'speakers/speaker24.html')
+
+
+def speakerDesc5(request):
+    return render(request, 'speakers/speaker25.html')
+
+
+def speakerDesc6(request):
+    return render(request, 'speakers/speaker26.html')
+
+
+def speakerDesc7(request):
+    return render(request, 'speakers/speaker27.html')
+
+
+def speakerDesc8(request):
     return render(request, 'speakers/speaker11.html')  
 
-def speakerDesc(request):
+
+def speakerDesc9(request):
     return render(request, 'speakers/speaker12.html')
-    
-def speakerDesc(request):
+
+
+def speakerDesc10(request):
     return render(request, 'speakers/speaker13.html')
-    
-def speakerDesc(request):
+
+
+def speakerDesc11(request):
     return rende(request, 'speakers/speaker14.html')
-    
-def speakerDesc(request):
+
+
+def speakerDesc12(request):
     return render(request, 'speakers/speaker15.html')
+
+
+def speakerDesc13(request):
+    return render(request, 'speakers/speaker16.html')
+
+
+
+
