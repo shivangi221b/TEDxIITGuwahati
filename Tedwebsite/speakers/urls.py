6552c0744fce451d81d2b60d5eb7e94c4aceda56
janhavilande <63102178+janhavilande@users.noreply.github.com>
2020-10-13 22:28:14 +0530

    update

--- Tedwebsite/speakers/urls.py
+++ Tedwebsite/speakers/urls.py
@@ -3,5 +3,20 @@ from . import views
 urlpatterns = [
     path('', views.speakers_home, name='speakers_home'),
     path('speaker1', views.speakers_home, name='speakers_home'),
+    path('/speaker21', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker22', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker23', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker24', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker25', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker26', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker27', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker11', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker12', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker13', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker14', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker15', views.speakerDesc, name = 'speaker_description'),
+    path('/speaker16', views.speakerDesc, name = 'speaker_description')
+    
+
     # path('add_speaker', views.addSpeaker),
 ]
--- Tedwebsite/speakers/views.py
+++ Tedwebsite/speakers/views.py
@@ -21,4 +21,23 @@ def addSpeaker(request):
 def nominate_yourself(request):
     return render(request, 'speakers/form2.html')
 
+def speakerDesc(request):
+    return render(request, 'speakers/speaker21.html')
 
+def speakerDesc(request):
+    return render(request, 'speakers/speaker22.html')
+    
+def speakerDesc(request):
+    return render(request, 'speakers/speaker11.html')  
+
+def speakerDesc(request):
+    return render(request, 'speakers/speaker12.html')
+    
+def speakerDesc(request):
+    return render(request, 'speakers/speaker13.html')
+    
+def speakerDesc(request):
+    return rende(request, 'speakers/speaker14.html')
+    
+def speakerDesc(request):
+    return render(request, 'speakers/speaker15.html')
