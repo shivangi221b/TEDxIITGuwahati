910f7e52830231651273e8516677f5568a4c18bf
janhavilande <63102178+janhavilande@users.noreply.github.com>
2020-10-24 18:22:12 +0530

    updated speakers and team members

--- Tedwebsite/about_us/views.py
+++ Tedwebsite/about_us/views.py
@@ -3,3 +3,108 @@ from django.shortcuts import render
 
 def about_us_home(request):
     return render(request, 'about_us/about_us.html')
+
+def memberDesc1(request):
+    return render(request, 'about_us/Member1.html')
+
+def memberDesc2(request):
+    return render(request, 'about_us/Member2.html')
+
+def memberDesc3(request):
+    return render(request, 'about_us/Member3.html')
+
+def memberDesc4(request):
+    return render(request, 'about_us/Member4.html')
+
+def memberDesc5(request):
+    return render(request, 'about_us/Member5.html')
+
+def memberDesc6(request):
+    return render(request, 'about_us/Member6.html')
+
+def memberDesc7(request):
+    return render(request, 'about_us/Member7.html')
+
+def memberDesc8(request):
+    return render(request, 'about_us/Member8.html')
+
+def memberDesc9(request):
+    return render(request, 'about_us/Member9.html')
+
+def memberDesc10(request):
+    return render(request, 'about_us/Member10.html')
+
+def memberDesc11(request):
+    return render(request, 'about_us/Member11.html')
+
+def memberDesc12(request):
+    return render(request, 'about_us/Member12.html')
+
+def memberDesc13(request):
+    return render(request, 'about_us/Member13.html')
+
+def memberDesc14(request):
+    return render(request, 'about_us/Member14.html')
+
+def memberDesc15(request):
+    return render(request, 'about_us/Member15.html')
+
+def memberDesc16(request):
+    return render(request, 'about_us/Member16.html')
+
+def memberDesc17(request):
+    return render(request, 'about_us/Member17.html')
+
+def memberDesc18(request):
+    return render(request, 'about_us/Member18.html')
+
+def memberDesc19(request):
+    return render(request, 'about_us/Member19.html')
+
+def memberDesc20(request):
+    return render(request, 'about_us/Member20.html')
+
+def memberDesc21(request):
+    return render(request, 'about_us/Member21.html')
+
+def memberDesc22(request):
+    return render(request, 'about_us/Member22.html')
+
+def memberDesc23(request):
+    return render(request, 'about_us/Member23.html')
+
+def memberDesc24(request):
+    return render(request, 'about_us/Member24.html')
+
+def memberDesc25(request):
+    return render(request, 'about_us/Member25.html')
+
+def memberDesc26(request):
+    return render(request, 'about_us/Member26.html')
+
+def memberDesc27(request):
+    return render(request, 'about_us/Member27.html')
+
+def memberDesc28(request):
+    return render(request, 'about_us/Member28.html')
+
+def memberDesc29(request):
+    return render(request, 'about_us/Member29.html')
+
+def memberDesc30(request):
+    return render(request, 'about_us/Member30.html')
+
+def memberDesc31(request):
+    return render(request, 'about_us/Member31.html')
+
+def memberDesc32(request):
+    return render(request, 'about_us/Member32.html')
+
+def memberDesc33(request):
+    return render(request, 'about_us/Member33.html')
+
+def memberDesc34(request):
+    return render(request, 'about_us/Member34.html')
+
+def memberDesc35(request):
+    return render(request, 'about_us/Member35.html')
