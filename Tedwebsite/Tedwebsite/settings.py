e9565b475770b896b86c525f35c2f859664ab64f
Shivangi Kumar <ireneholmes221999@gmail.com>
2020-10-31 15:51:02 +0530

    templates updated

--- Tedwebsite/Tedwebsite/settings.py
+++ Tedwebsite/Tedwebsite/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = '+l4hm@bcff)7v0xk17&bzjtyj*%_)p4jfsd&3!oirvhnaiubho'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-13-233-160-244.ap-south-1.compute.amazonaws.com']
-
+ALLOWED_HOSTS = ['ec2-13-233-160-244.ap-south-1.compute.amazonaws.com', '127.0.0.1']
 
 # Application definition
 
@@ -57,14 +55,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Tedwebsite.urls'
 
-
-
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
-	'DIRS': [BASE_DIR + "/templates", ],
+        'DIRS': ['templates', ],
+        'DIRS': [BASE_DIR + "/templates", ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Tedwebsite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -89,7 +83,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -109,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -123,22 +115,19 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 
-
 STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
 
-STATIC_URL = '/static/' # You may find this is already defined as such.
+STATIC_URL = '/static/'  # You may find this is already defined as such.
 
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
